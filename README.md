9f54645b6927e9999e933a85fc2acb211a89f3ed
Nicholaus Rowe <ux@nicholausrowe.com>
2019-09-26 02:55:38 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# To-Do List
+
+noderun to-do list
---
+++ todoApp.js
@@ -0,0 +1,35 @@
+
+function app() {
+
+  pjs.defineDisplay("display", "todo.json");
+
+  var list = pjs.query("SELECT * FROM todo")
+
+  display.grid.replaceRecords(list);
+
+  while (true) {
+
+    display.toDoScreen.execute();
+    if (add) addItem(display.grid, newItem);
+    removeItems(display.grid);
+  }
+
+}
+
+function addItem(grid, newItem) {
+  grid.push({item: newItem});
+  pjs.query("INSERT INTO todo SET ?", {item: newItem});
+}
+
+
+function removeItems(grid) {
+
+  var recordsToRemove = grid.filter(entry => entry.remove);
+
+  recordsToRemove.forEach(record => pjs.query("DELETE FROM todo WHERE item = ?", record.item));
+
+  grid.applyFilter(entry => !entry.remove);
+
+}
+
+exports.default = app;
